74eb9661d660c6edc5ff7fde57e757693f10dd35
Matt Couch <quikboy87@live.com>
2017-10-16 16:09:56 +1100

    Connected Firebase and ability to add vehicles!

--- app/src/main/java/inft3970/fuelapp/FuelMapActivity.java
+++ app/src/main/java/inft3970/fuelapp/FuelMapActivity.java
@@ -199,33 +199,6 @@ public class FuelMapActivity extends AppCompatActivity implements OnMapReadyCall
             }
         });
     }
-/*
-    @Override
-    public void onStart() {
-        super.onStart();
-
-        OptionalPendingResult<GoogleSignInResult> opr = Auth.GoogleSignInApi.silentSignIn(mGoogleApiClient);
-        if (opr.isDone()) {
-            // If the user's cached credentials are valid, the OptionalPendingResult will be "done"
-            // and the GoogleSignInResult will be available instantly.
-            Log.d(TAG, "Got cached sign-in");
-            GoogleSignInResult result = opr.get();
-            handleSignInResult(result);
-        } else {
-            // If the user has not previously signed in on this device or the sign-in has expired,
-            // this asynchronous branch will attempt to sign in the user silently.  Cross-device
-            // single sign-on will occur in this branch.
-            //showProgressDialog();
-            opr.setResultCallback(new ResultCallback<GoogleSignInResult>() {
-                @Override
-                public void onResult(GoogleSignInResult googleSignInResult) {
-                    //hideProgressDialog();
-                    handleSignInResult(googleSignInResult);
-                }
-            });
-        }
-    }
-*/
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
@@ -274,13 +247,7 @@ public class FuelMapActivity extends AppCompatActivity implements OnMapReadyCall
 
 
     /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
+     * Does stuff
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
--- app/src/main/java/inft3970/fuelapp/FuelStopActivity.java
+++ app/src/main/java/inft3970/fuelapp/FuelStopActivity.java
@@ -2,12 +2,29 @@ package inft3970.fuelapp;
 
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static inft3970.fuelapp.FuelMapActivity.googleID;
 
@@ -22,31 +39,106 @@ public class FuelStopActivity extends Activity implements AdapterView.OnItemSele
     Context context;
     String selectedType;
     String userID = "";
-    String[] vehicleNames = new String[2];
+    EditText vehicleNameInput;
+    List<String> vehicleNames = new ArrayList<>();
+    Button addVehicleButton;
+
+    Firebase firebaseReference;
+    DataSnapshot dataSnapshot;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fuel_stop);
         context = App.getContext();
-
+        Firebase.setAndroidContext(this);
 
         userID = googleID;
 
+        firebaseReference = new Firebase("https://inft3970-major-p-1503830364825.firebaseio.com/");
 
-        //Dummy values, loop here to read all.
-        vehicleNames[0] = userID;
-        vehicleNames[1] = "Two";
+        firebaseReference.addValueEventListener((new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
 
-        //set visibility based on if no vehicles are available, only show button and addVehicle section
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        }));
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        vehicleNames.clear();
+
+        firebaseReference.child(googleID).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String firstChild;
+                for (DataSnapshot postsnapshot : dataSnapshot.getChildren()) {
+                    firstChild = postsnapshot.getKey();
+                    vehicleNames.add(firstChild);
+                }
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        });
+
+        addVehicleButton = (Button)findViewById(R.id.addVehicleButton);
+        vehicleNameInput = (EditText)findViewById(R.id.vehicleNameInput);
+        addVehicleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog errorDialog = createNoSavedDataDialog(null);
+                String inputName = vehicleNameInput.getText().toString();
+                if (!inputName.equals("")) {
+                    for (int i = 0; i < vehicleNames.size(); i++) {
+                        if (inputName.equals(vehicleNames.get(i))) {
+                            errorDialog.show();
+                        } else {
+                            firebaseReference.child(googleID).child(inputName).child("FuelStop").child("VisitID").setValue("0");
+                            finish();
+                        }
+                    }
+                } else {
+                    errorDialog.show();
+                }
+            }
+        });
+
 
         vehicleList = (Spinner)findViewById(R.id.vehicleList);
-
         //Get the list of vehicles under the userID, and display in a dropdown menu
         vehicleList.setOnItemSelectedListener(this);
         ArrayAdapter<String> VehicleList = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, vehicleNames);
         vehicleList.setAdapter(VehicleList);
+    }
 
+    public AlertDialog createNoSavedDataDialog(Bundle savedInstanceState) {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(FuelStopActivity.this);
+        builder.setTitle("Warning");
+        builder.setMessage("Vehicle name is invalid.");
+        builder.setPositiveButton("Return", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        builder.setNegativeButton("Exit", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+        });
+        return builder.create();
     }
 
     @Override
