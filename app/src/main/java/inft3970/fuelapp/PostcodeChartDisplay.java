0a3aa8674542fe89dc3d277bf02e7cd0c0e20b7e
Shane Vincent <shane.vincent@hotmail.com>
2017-11-03 14:15:56 +1100

    New Changes - FuelCodeNameCall - PlaceDetails - PlaceDetailsCall - PlaceIDCall - ReviewAdapter - SplashScreen
    Edited
    - App
    - FuelFilterActivity
    - FuelMapActivity
    - FuelPriceAdapter
    - RVAdapter
    - StationActivity
    - StationByRadiusCall
    - Gradle Build

--- app/src/main/java/inft3970/fuelapp/PostcodeChartDisplay.java
+++ app/src/main/java/inft3970/fuelapp/PostcodeChartDisplay.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.FragmentActivity;
 import android.view.View;
-
 import android.widget.ImageView;
 import android.widget.Toast;
 
